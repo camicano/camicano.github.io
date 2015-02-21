ec015e2b61fffecdd515ea97d86d293b08ff8dbc
Camila Cano <camilacanog@gmail.com>
2015-02-21 12:17:53 -0500

    update links

--- scripts/backbone/routers/Router.js
+++ scripts/backbone/routers/Router.js
@@ -8,8 +8,8 @@ app.Router = Backbone.Router.extend({
 
   initialize: function() {
     this.projects = new app.ProjectList([
-      new app.Project({title: 'White Paradise', link: 'http://54.201.33.116:3000', image: 'images/white.png', description: 'A story about the relationship between the production of cocaine and the war in Colombia', techspecs: [ 'Rails 4', 'Ruby', 'Mongo DB', 'JavaScript', 'D3' ], github: 'https://github.com/camicano/infographic' }),
-      new app.Project({title: 'Food Me', link: 'http://foodme.us/', image: 'images/foodme.png', description: 'A platform that allows you to see where are food truck located near your area.', techspecs: [ 'Rails', 'Ruby', 'Javascript', 'jQuery', 'Postgresql', 'Googlemaps API', 'Frousquare API', 'Skeleton' ], github: 'https://github.com/camicano/food_trucks_app' }),
+      new app.Project({title: 'White Paradise', link: 'https://floating-citadel-7662.herokuapp.com/', image: 'images/white.png', description: 'A story about the relationship between the production of cocaine and the war in Colombia', techspecs: [ 'Rails 4', 'Ruby', 'Mongo DB', 'JavaScript', 'D3' ], github: 'https://github.com/camicano/infographic' }),
+      new app.Project({title: 'Food Me', link: 'https://floating-reaches-8820.herokuapp.com/', image: 'images/foodme.png', description: 'A platform that allows you to see where are food truck located near your area.', techspecs: [ 'Rails', 'Ruby', 'Javascript', 'jQuery', 'Postgresql', 'Googlemaps API', 'Frousquare API', 'Skeleton' ], github: 'https://github.com/camicano/food_trucks_app' }),
       new app.Project({title: 'Asana', link: 'http://yogapp.herokuapp.com/', image: 'images/asana.png', description: 'An app that allows you to find affordable yoga classes near your location and at a time that works for you.', techspecs: [ 'Rails', 'Ruby', 'Postgresql', 'Skeleton' ], github: 'https://github.com/camicano/yoga_app' })
     ]);
 
