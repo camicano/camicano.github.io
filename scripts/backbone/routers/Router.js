c4479c83b9be1eea5b7f39d750cab418376eeb1a
camicano <camilacanog@gmail.com>
2016-02-01 08:51:26 -0800

    minor edit

--- scripts/backbone/routers/Router.js
+++ scripts/backbone/routers/Router.js
@@ -8,9 +8,9 @@ app.Router = Backbone.Router.extend({
 
   initialize: function() {
     this.projects = new app.ProjectList([
-      new app.Project({title: 'White Paradise', link: 'https://floating-citadel-7662.herokuapp.com/', image: 'images/white.png', description: 'A story about the relationship between the production of cocaine and the war in Colombia', techspecs: [ 'Rails 4', 'Ruby', 'Mongo DB', 'JavaScript', 'D3' ], github: 'https://github.com/camicano/infographic' }),
       new app.Project({title: 'Food Me', link: 'https://floating-reaches-8820.herokuapp.com/', image: 'images/foodme.png', description: 'A platform that allows you to see where are food truck located near your area.', techspecs: [ 'Rails', 'Ruby', 'Javascript', 'jQuery', 'Postgresql', 'Googlemaps API', 'Frousquare API', 'Skeleton' ], github: 'https://github.com/camicano/food_trucks_app' }),
-      new app.Project({title: 'Asana', link: 'http://yogapp.herokuapp.com/', image: 'images/asana.png', description: 'An app that allows you to find affordable yoga classes near your location and at a time that works for you.', techspecs: [ 'Rails', 'Ruby', 'Postgresql', 'Skeleton' ], github: 'https://github.com/camicano/yoga_app' })
+      new app.Project({title: 'Asana', link: 'http://yogapp.herokuapp.com/', image: 'images/asana.png', description: 'An app that allows you to find affordable yoga classes near your location and at a time that works for you.', techspecs: [ 'Rails', 'Ruby', 'Postgresql', 'Skeleton' ], github: 'https://github.com/camicano/yoga_app' }),
+      new app.Project({title: 'White Paradise', link: '#', image: 'images/white.png', description: 'A story about the relationship between the production of cocaine and the war in Colombia (This app is currently not live)', techspecs: [ 'Rails 4', 'Ruby', 'Mongo DB', 'JavaScript', 'D3' ], github: 'https://github.com/camicano/infographic' })
     ]);
 
     this.topmenu = new app.TopMenuList([
